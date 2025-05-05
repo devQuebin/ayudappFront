36a73bc168b3cf92d7feaa29d38e18200243de0c
lazaronazareno <lazaronazareno@gmail.com>
2025-05-05 18:18:17 -0300

    campaigns, register and login pages (#1)
    * merge development into lazaro without linux files that generates conflict
    
    * campaigns page with card component
    
    * register and login form with pages, react-hook-form added and navbar links updated
    
    * merge development
    
    * Delete public/img/fd9e1149ce79bab941fc121bb15865a2.jpg:Zone.Identifier
    
    * Delete src/app/favicon.ico:Zone.Identifier

---
+++ src/interfaces/ICampaign.interface.tsx
@@ -0,0 +1,19 @@
+export interface ICampaign {
+  id: string;
+  name: string;
+  start_date: string;
+  due_date: string;
+  updated_at: {
+    seconds: number;
+    nanoseconds: number;
+  };
+  amount_target: number;
+  owner_id: string;
+  description: string;
+  end_date: string;
+  status: string;
+  created_at: {
+    seconds: number;
+    nanoseconds: number;
+  };
+}
